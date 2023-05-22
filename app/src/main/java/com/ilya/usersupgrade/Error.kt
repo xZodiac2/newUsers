453753ba8d6e6f1f9acc9292b83a2eae543c0ad1
xZodiac2 <xirenoa@gmail.com>
2023-05-22 23:48:17 +0400

    commit

--- app/src/main/java/com/ilya/usersupgrade/Error.kt
+++ app/src/main/java/com/ilya/usersupgrade/Error.kt
@@ -4,4 +4,5 @@ sealed class Error (
     override val message: String
 ) : Throwable(message) {
     object InvalidInputError : Error("Invalid login or password")
+    object UserNameIsUndefined : Error("Invalid meaning of userName property: null")
 }
--- app/src/main/java/com/ilya/usersupgrade/UserGreetingActivity.kt
+++ app/src/main/java/com/ilya/usersupgrade/UserGreetingActivity.kt
@@ -8,20 +8,49 @@ import com.ilya.usersupgrade.databinding.ActivityUserGreetingBinding
 class UserGreetingActivity : AppCompatActivity() {
 
     private lateinit var activityUserGreetingViews: ActivityUserGreetingBinding
+    private lateinit var nameOfSignedUser: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityUserGreetingViews = ActivityUserGreetingBinding.inflate(layoutInflater)
         setContentView(activityUserGreetingViews.root)
 
-        val userName = intent.getStringExtra("userName")
-        activityUserGreetingViews.tvGreeting.text = "${activityUserGreetingViews.tvGreeting.text} $userName"
+        nameOfSignedUser = intent.getStringExtra("userName") ?: "undefined"
+
+        when (nameOfSignedUser) {
+            "undefined" -> {
+                toUIWithError()
+                mapError(Error.UserNameIsUndefined.message)
+            }
+            else -> activityUserGreetingViews.tvGreeting.text = "${activityUserGreetingViews.tvGreeting.text} $nameOfSignedUser"
+        }
 
         activityUserGreetingViews.btnUnlogin.setOnClickListener(this::unlogin)
     }
 
+    private fun mapError(error: String) {
+        activityUserGreetingViews.tvErrorUserIsNull.text = error
+    }
+
+    private fun toUIWithError() {
+        activityUserGreetingViews.tvErrorUserIsNull.show()
+        activityUserGreetingViews.tvGreeting.hide()
+    }
+
+    private fun toDefaultUI() {
+        activityUserGreetingViews.tvGreeting.show()
+        activityUserGreetingViews.tvErrorUserIsNull.hide()
+    }
+
+
     private fun unlogin(view: View) {
         finish()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        toDefaultUI()
     }
 
 }
