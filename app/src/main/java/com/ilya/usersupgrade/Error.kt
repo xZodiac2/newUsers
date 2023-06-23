786bb6ea5030755927b665297b410ca82f6355c1
xZodiac2 <xirenoa@gmail.com>
2023-06-23 15:06:44 +0400

    add input checking

--- app/src/main/java/com/ilya/usersupgrade/Error.kt
+++ app/src/main/java/com/ilya/usersupgrade/Error.kt
@@ -3,4 +3,12 @@ package com.ilya.usersupgrade
 
 sealed class Error(val stringId: Int) : Throwable() {
     object InvalidInputError : Error(R.string.text_error_invalid_input)
+    
+    sealed class InputError(stringId: Int) : Error(stringId) {
+        object PasswordsDoNotMatchError : InputError(R.string.text_registration_passwords_do_not_match_error)
+        object PasswordIsShort : InputError(R.string.text_registration_password_is_short)
+        object LengthError : InputError(R.string.text_registration_input_length_error)
+        object EmptyFieldError : InputError(R.string.text_registration_empty_field_error)
+        
+    }
 }
--- app/src/main/java/com/ilya/usersupgrade/RegistrationActivity.kt
+++ app/src/main/java/com/ilya/usersupgrade/RegistrationActivity.kt
@@ -1,8 +1,11 @@
 package com.ilya.usersupgrade
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.ilya.usersupgrade.databinding.ActivityRegistrationBinding
 
@@ -22,9 +25,44 @@ class RegistrationActivity : AppCompatActivity() {
     }
     
     private fun register(view: View) = with(views) {
+        removeErrors()
+        
         val userName = etName.text.toString()
         val userLogin = etLogin.text.toString()
         val userPassword = etPassword.text.toString()
+        val userRepeatedPassword = etRepeatedPassword.text.toString()
+        
+        val maxInputLength = 25
+    
+        if (userName.isEmpty()) {
+            onInputError(Error.InputError.EmptyFieldError, etName)
+            return
+        }
+        
+        if (userLogin.isEmpty()) {
+            onInputError(Error.InputError.EmptyFieldError, etLogin)
+            return
+        }
+        
+        if (userName.length > maxInputLength) {
+            onInputError(Error.InputError.LengthError, etName)
+            return
+        }
+        
+        if (userLogin.length > maxInputLength) {
+            onInputError(Error.InputError.LengthError, etLogin)
+            return
+        }
+        
+        if (userPassword.length < 8) {
+            onPasswordsError(Error.InputError.PasswordIsShort, etPassword)
+            return
+        }
+        
+        if (userRepeatedPassword != userPassword) {
+            onPasswordsError(Error.InputError.PasswordsDoNotMatchError, etRepeatedPassword)
+            return
+        }
         
         val user = User(userName, userLogin, userPassword)
         
@@ -33,6 +71,42 @@ class RegistrationActivity : AppCompatActivity() {
         Toast.makeText(this@RegistrationActivity, getString(R.string.text_registration_user_added_successfully), Toast.LENGTH_SHORT).show()
         
         finish()
+    }
+    
+    private fun onPasswordsError(passwordError: Error, passwordField: View) = with(views) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        
+        imm.showSoftInput(passwordField, InputMethodManager.SHOW_IMPLICIT)
+    
+        passwordField.requestFocus()
+        
+        tvErrorPassword.text = passwordError.extract(this@RegistrationActivity)
+        tvErrorPassword.visibility = View.VISIBLE
+        
+        etPassword.inputType = InputType.TYPE_CLASS_TEXT
+        etRepeatedPassword.inputType = InputType.TYPE_CLASS_TEXT
+    }
+    
+    private fun onInputError(inputError: Error, inputField: View) = with(views) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        
+        imm.showSoftInput(inputField, InputMethodManager.SHOW_IMPLICIT)
+        
+        inputField.requestFocus()
+    
+        (when (inputField) {
+            etName -> tvNameLengthError
+            else -> tvLoginLengthError
+        }).apply {
+            text = inputError.extract(this@RegistrationActivity)
+            visibility = View.VISIBLE
+        }
+    }
+    
+    private fun removeErrors() = with(views) {
+        tvErrorPassword.visibility = View.GONE
+        tvNameLengthError.visibility = View.GONE
+        tvLoginLengthError.visibility = View.GONE
     }
     
 }
