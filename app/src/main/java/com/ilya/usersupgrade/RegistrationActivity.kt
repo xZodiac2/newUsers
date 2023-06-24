5a4407d1bc4b4f0489b621303d1e8cd9429ce084
xZodiac2 <xirenoa@gmail.com>
2023-06-25 02:20:41 +0400

    delete excess imports

--- app/src/main/java/com/ilya/usersupgrade/RegistrationActivity.kt
+++ app/src/main/java/com/ilya/usersupgrade/RegistrationActivity.kt
@@ -2,9 +2,6 @@ package com.ilya.usersupgrade
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
